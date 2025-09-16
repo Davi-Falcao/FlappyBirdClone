cc2799096e9ddcb92ae383297a9feeea717c08a9
Davi Falcao <davifaf3106@gmail.com>
2025-09-15 22:37:30 -0300

    Adicionando comentarios

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ def create_pipe():
     random_pipe_pos = random.choice(pipe_height)
     bottom_pipe = pipe_surface.get_rect(midtop = (600, random_pipe_pos -50))
     top_pipe = pipe_surface.get_rect(midbottom = (600, random_pipe_pos -400))
+    
     return bottom_pipe, top_pipe
 
 def move_pipes(pipes):
     for pipe in pipes:
         pipe.centerx -= 5
     visible_pipes = [pipe for pipe in pipes if pipe.right > -50]
+    
     return visible_pipes
 
 def draw_pipe(pipes):
@@ -26,29 +28,33 @@ def draw_pipe(pipes):
 
 def check_collision(pipes):
     global can_score
-
+    resultado = True
     for pipe in pipes:
         if bird_rect.colliderect(pipe):
             death_sound.play()
             can_score = True
-            return False
+            resultado = False
 
     if bird_rect.top <= -150 or bird_rect.bottom >= 720:
         can_score = True
-        return False
+        resultado = False
 
-    return True
+    return resultado
 
 def rotate_bird(bird):
     new_bird = pygame.transform.rotozoom(bird, -bird_movement *3,1)
+    
     return new_bird
 
 def bird_animation():
     new_bird = bird_frames[bird_index]
+    #Atualiza animacao do passaro
     new_bird_rect = new_bird.get_rect(center = (100, bird_rect.centery))
+    
     return new_bird, new_bird_rect
 
 def score_display(game_state):
+    #Atualiza tela durante o jogo
     if game_state == 'main_game':
         score_surface = game_font.render(str(int(score)), True, (83,53,69))
         score_rect = score_surface.get_rect(center = (250, 100))
@@ -58,7 +64,9 @@ def score_display(game_state):
         score_rect_black = score_surface_black.get_rect(center=(245, 95))
         screen.blit(score_surface_black, score_rect_black)
 
+    #Atualiza tela quando morre
     if game_state == 'game_over':
+
         score_surface = game_font.render(f'Score: {int(score)}', True, (255, 255, 255))
         score_rect = score_surface.get_rect(center=(250, 50))
         screen.blit(score_surface, score_rect)
@@ -80,6 +88,7 @@ def update_score(score, high_score):
         high_score = score
     return high_score
 
+#Atualiza a pontuacao a cada cano passado
 def pipe_score_check():
     global score,can_score
 
@@ -119,6 +128,7 @@ floor_surface = pygame.image.load('Data/base.png').convert()
 floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_pos = 0
 
+#Variaveis do passaro
 bird_downflap = pygame.transform.scale2x(pygame.image.load('Data/bluebird-downflap.png').convert_alpha())
 bird_midflap = pygame.transform.scale2x(pygame.image.load('Data/bluebird-midflap.png').convert_alpha())
 bird_upflap = pygame.transform.scale2x(pygame.image.load('Data/bluebird-upflap.png').convert_alpha())
@@ -130,7 +140,7 @@ bird_rect = bird_surface.get_rect(center = (100, 360))
 BIRDFLAP = pygame.USEREVENT + 1
 pygame.time.set_timer(BIRDFLAP, 200)
 
-
+#Variaveis do cano
 pipe_surface = pygame.image.load('Data/pipe-green.png')
 pipe_surface = pygame.transform.scale2x(pipe_surface)
 pipe_list = []
@@ -138,10 +148,12 @@ SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
 pipe_height = [400, 600, 800]
 
+#Imagens Game Over
 game_over_surface = pygame.image.load('Data/message.png').convert_alpha()
 game_over_surface = pygame.transform.scale2x(game_over_surface)
 game_over_rect = game_over_surface.get_rect(center = (250, 360))
 
+#Sons do jogo
 flap_sound = pygame.mixer.Sound('Data/sfx_wing.wav')
 death_sound = pygame.mixer.Sound('Data/sfx_die.wav')
 score_sound = pygame.mixer.Sound('Data/sfx_point.wav')
@@ -149,9 +161,11 @@ score_sound_countdown = 100
 
 while True:
     for event in pygame.event.get():
+        #Fecha o jogo
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        #Atualiza o movimento do passaro com o espaco 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and game_active == True:
                 bird_movement = 0
@@ -163,10 +177,12 @@ while True:
                 bird_rect.center = (100,360)
                 bird_movement = 0
                 score = 0
-
+        
+        #Cria canos
         if event.type == SPAWNPIPE:
             pipe_list.extend(create_pipe())
 
+        #Passaro batendo as asas
         if event.type == BIRDFLAP:
             if bird_index < 2:
                  bird_index += 1
@@ -174,7 +190,7 @@ while True:
                 bird_index = 0
 
             bird_surface, bird_rect = bird_animation()
-
+    
     screen.blit(bg_surface,(0,0))
 
     if game_active:
